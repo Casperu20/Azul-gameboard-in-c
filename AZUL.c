cc29c6573972b8b33f4011f00fadc124ed3e18f9
Casperu20 <nicubunea20@gmail.com>
2025-05-29 11:38:08 +0300

    Milestone 3: Added middle area, color-coded tile display, and enhanced board visualization

--- AZUL.c
+++ AZUL.c
@@ -1,4 +1,4 @@
-// MILESTONE 2:    BUNEA NICOLAE
+// MILESTONE 3:    BUNEA NICOLAE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,6 +14,7 @@
 #define MAX_PATTERN_LINES 5
 #define WALL_SIZE 5
 #define FLOOR_LINE_MAX 7
+#define MIDDLE_SIZE 101
 
 typedef struct{
   int patternLines[MAX_PATTERN_LINES][MAX_PATTERN_LINES];
@@ -31,12 +32,14 @@ typedef struct{
   PLAYER players[2]; // Made this game only for 2 players
   int factoryDisplays[FACTORY_DISPLAYS][TILES_PER_FACTORY];
   int middleAreaHasFirstPlayerToken; // 0 = No, 1 = Yes
+  int middleArea[MIDDLE_SIZE];
 }GAME_STATE;
 
 void initialize_player_board(PLAYER_BOARD* board){
   // I'll set default values at the start:
   memset(board->patternLines, -1, sizeof(board->patternLines)); // -1 - EMPTY
   memset(board->wall, 0, sizeof(board->wall)); // 0 - EMPTY
+  // Score marker & floor
   memset(board->floorLine, -1, sizeof(board->floorLine)); // -1 - EMPTY
   board->score = 0;
 }
@@ -60,7 +63,7 @@ void initialize_bag(int bag[]){
 }
 // I tried to use rand() to randomised the tiles, seems to work fine
 void shuffle_bag(int bag[], int size){
-  printf("\nBag contents after rand():  [SIZE = %d]\n", size);
+  printf("\n  DEBUG --> Bag contents after rand():  [SIZE = %d]\n", size);
   for (int i = size - 1; i > 0; --i){
     int j = rand() % (i + 1);
     int temp = bag[i];
@@ -69,6 +72,37 @@ void shuffle_bag(int bag[], int size){
     printf("%d, ", bag[i]);
   }
   printf("\n");
+}
+
+void print_tile(int tileType){
+  const char* colors[NUMBER_OF_TYPES] = {
+    "\x1b[34m", // Blue
+    "\x1b[33m", // Yellow
+    "\x1b[31m", // Red
+    "\x1b[38;5;16m", // Black
+    "\x1b[37m"  // White
+  };
+  
+  if(tileType >= 0 && tileType < NUMBER_OF_TYPES){
+    if(tileType == 0){
+      printf("%s[Blue]\x1b[0m", colors[tileType]); 
+    }
+    else if(tileType == 1){
+      printf("%s[Yellow]\x1b[0m", colors[tileType]); 
+    }
+    else if(tileType == 2){
+      printf("%s[Red]\x1b[0m", colors[tileType]); 
+    }
+    else if(tileType == 3){
+      printf("%s[Black]\x1b[0m", colors[tileType]); 
+    }
+    else{
+      printf("%s[White]\x1b[0m", colors[tileType]); 
+    }
+  }
+  else{
+    printf("   ");
+  }
 }
 
 void fill_factory_displays(GAME_STATE* game, int bag[], int* size){
@@ -80,19 +114,46 @@ void fill_factory_displays(GAME_STATE* game, int bag[], int* size){
     }
   }
   // Printing:
-  printf("\n0 = Blue | 1 = Yellow | 2 = Red | 3 = Black | 4 = White | X = First player token\n");
+  printf("\n");
   for (int i = 0; i < FACTORY_DISPLAYS; ++i){
     printf("Factory %d: ", i + 1);
     for (int j = 0; j < TILES_PER_FACTORY; ++j){
-      printf("%d ", game->factoryDisplays[i][j]);
+      //printf("%d ", game->factoryDisplays[i][j]);
+      print_tile(game->factoryDisplays[i][j]);
+      printf(" "); 
     }
     printf("\n");
   }
-  printf("  --> Tiles remained in bag: %d\n", *size);
+  /*printf("Middle Area: ");
+  if(game->middleAreaHasFirstPlayerToken){
+    printf("X\n");
+  } 
+  else{
+    printf("NO 1st Player Token\n");
+  }*/
+  
+  printf("\n  DEBUG --> Tiles remained in bag: %d\n", *size);
+  printf("Remaining bag contents: ");
+  for(int i = 0; i < *size; i++){
+    printf("%d, ", bag[i]);
+  }
+  printf("\n\n");
+  
+}
+
+void display_middle_area(GAME_STATE* game){
+  printf("Middle Area: ");
+  if(game->middleAreaHasFirstPlayerToken == 1){
+    printf("X\n");
+  } 
+  else{
+    printf("NO 1st Player Token\n");
+  }
 }
 
 void player_choose_tiles(GAME_STATE* game, int playerIndex){
   int factoryDisplay, tileColor;
+  printf("\n0 = Blue | 1 = Yellow | 2 = Red | 3 = Black | 4 = White | X = First player token\n");
   printf("\n%s, choose a factory display (1-5): ", game->players[playerIndex].player_name);
   scanf("%d", &factoryDisplay);
   while (factoryDisplay < 1 || factoryDisplay > 5){
@@ -100,7 +161,7 @@ void player_choose_tiles(GAME_STATE* game, int playerIndex){
     scanf("%d", &factoryDisplay);
   }
   
-  printf("Select what tile you want: ");
+  printf("Select what tile you want (0-4): ");
   scanf("%d", &tileColor);
   while (tileColor < 0 || tileColor >= NUMBER_OF_TYPES){
     printf("Invalid choice. Please choose a tile color (0=Blue, 1=Yellow, 2=Red, 3=Black, 4=White): ");
@@ -111,8 +172,85 @@ void player_choose_tiles(GAME_STATE* game, int playerIndex){
 }
 
 void display_player_board(const PLAYER_BOARD* board){
-  // All this mass is for putting the pattern lines near the wall
-  int maxRows = WALL_SIZE > MAX_PATTERN_LINES ? WALL_SIZE : MAX_PATTERN_LINES;
+  printf("  --> Score marker: %d\n\n", board->score);
+
+  printf("Pattern Lines:\n");
+  for (int i = 0; i < MAX_PATTERN_LINES; i++){
+    for (int j = 0; j <= i; j++){
+      if (board->patternLines[i][j] == -1)
+        printf("%2d ", board->patternLines[i][j]); // empty yet
+      else 
+        print_tile(board->patternLines[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\nWall:\n");
+  for (int i = 0; i < WALL_SIZE; i++){
+    for (int j = 0; j < WALL_SIZE; j++) {
+      if (board->wall[i][j] == 0)
+        printf("%2d ", board->wall[i][j]);
+      else 
+        print_tile(board->wall[i][j]);
+    }
+    printf("\n");
+  }
+  
+  printf("\nFloor Line: ");
+  for (int i = 0; i < FLOOR_LINE_MAX; i++){
+    if (board->floorLine[i] == -1){
+      printf(" [EMPTY] ");
+      //printf("%d ", board->floorLine[i]);
+    } 
+    else{
+      print_tile(board->floorLine[i]);
+      printf(" ");
+    }
+  }
+  printf("\n\n(Here will be the tile placing section)\n");
+}
+
+#define PLAYER_TOKEN 5
+void initialize_game_state(GAME_STATE* game){
+  // Initialize players
+  initialize_players(&game->players[0], &game->players[1]);
+  // Place the 1st player token
+  for (int i = 0; i < MIDDLE_SIZE; ++i) {
+    game->middleArea[i] = -1; // Initializiing empty
+  }
+  
+  game->middleArea[0] = PLAYER_TOKEN;
+  game->middleAreaHasFirstPlayerToken = 1;
+  // Intiliaze and randomize the bag, after it fills the facotry displays
+  int bag[BAG_SIZE];
+  initialize_bag(bag);
+  int size = BAG_SIZE;
+  shuffle_bag(bag, size);
+  fill_factory_displays(game, bag, &size);
+}
+
+int main(){
+  srand(time(NULL));
+    
+  GAME_STATE game;
+  initialize_game_state(&game);
+  
+  display_middle_area(&game);
+    
+  for (int i = 0; i < 2; ++i){
+    player_choose_tiles(&game, i);
+  }
+    
+  // For gameboards:
+  for (int i = 0; i < 2; ++i){
+    printf("\n%s's Board:\n", game.players[i].player_name);
+    display_player_board(&game.players[i].board);
+  }
+
+  return 0;
+}
+
+/* OLD DISPLAYING FOR PATTERN LINE + WALL
+int maxRows = WALL_SIZE > MAX_PATTERN_LINES ? WALL_SIZE : MAX_PATTERN_LINES;
   for (int i = 0; i < maxRows; i++){
     if (i < MAX_PATTERN_LINES){
       for (int j = 0; j <= i; j++){
@@ -132,7 +270,7 @@ void display_player_board(const PLAYER_BOARD* board){
     }
     printf(" | ");
     if (i < WALL_SIZE){
-      for (int j = 0; j < WALL_SIZE; j++) {
+      for (int j = 0; j < WALL_SIZE; j++){
         if (board->wall[i][j] == 0)
           printf("%2d ", board->wall[i][j]);
         else 
@@ -141,45 +279,4 @@ void display_player_board(const PLAYER_BOARD* board){
     }
     printf("\n");
   }
-  printf("\n(Pattern lines  |   Wall)\n");
-}
-
-void initialize_game_state(GAME_STATE* game){
-  // Initialize players
-  initialize_players(&game->players[0], &game->players[1]);
-  // Place the 1st player token
-  game->middleAreaHasFirstPlayerToken = 1;
-  // Intiliaze and randomize the bag, after it fills the facotry displays
-  int bag[BAG_SIZE];
-  initialize_bag(bag);
-  int size = BAG_SIZE;
-  shuffle_bag(bag, size);
-  fill_factory_displays(game, bag, &size);
-}
-
-int main(){
-  srand(time(NULL));
-    
-  GAME_STATE game;
-  initialize_game_state(&game);
-  printf("Middle Area: ");
-  if(game.middleAreaHasFirstPlayerToken){
-    printf("X\n");
-  } 
-  else{
-    printf("NO 1st Player Token\n");
-  }
-    
-  for (int i = 0; i < 2; ++i){
-    player_choose_tiles(&game, i);
-  }
-    
-  // For gameboards:
-  for (int i = 0; i < 2; ++i){
-    printf("\n%s's Board:\n", game.players[i].player_name);
-    display_player_board(&game.players[i].board);
-  }
-
-  return 0;
-}
-
+*/
